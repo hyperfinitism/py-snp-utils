0acfa509ebda5b4a38671a42ee26430769063b3a
Takuma IMAMURA <209989118+hyperfinitism@users.noreply.github.com>
2025-12-22 15:12:31 +0900

    fix(types,examples): support V2 report

--- examples/display.py
+++ examples/display.py
@@ -14,12 +14,12 @@ DEFAULT_REPORT_PATH = os.path.join(os.path.dirname(__file__), "reportV3.bin")
 
 def main():
     input_path = input(f"Enter the path to the report file (default: {DEFAULT_REPORT_PATH}): ") or DEFAULT_REPORT_PATH
+    proc_model = input(f"Enter the processor model (default: autodetect): ") or None
     with open(input_path, "rb") as f:
-        report_v3 = f.read()
-    parsed_report_v3 = AttestationReport.from_bytes(report_v3)
-    print(json.dumps(parsed_report_v3.to_dict(), indent=4))
-    proc_model = parsed_report_v3.get_processor_model()
-    print(f"Processor model: {proc_model}")
+        report_bin = f.read()
+    parsed_report = AttestationReport.from_bytes(report_bin, processor_model=proc_model)
+    print(json.dumps(parsed_report.to_dict(), indent=4))
+    print(f"Processor model: {parsed_report.processor_model}")
 
 
 if __name__ == "__main__":
